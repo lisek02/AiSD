2e2ab51599a1bbf5d6a4bb059c27173d299bd97c
Wojciech Lisiecki <wojciech.j.lisiecki@gmail.com>
2014-03-31 15:24:17 +0200

    tworzenie drzewa AVL z listy

--- avl.c
+++ avl.c
@@ -4,6 +4,14 @@
 
 int i,size;
 
+struct avl
+{
+    struct avl *p;
+    struct avl *left;
+    struct avl *right;
+    int val, bf;
+};
+typedef struct avl avlNode;
 
 void losuj(int *tab)
 {
@@ -22,16 +30,73 @@ void losuj(int *tab)
 void wyswietl(int *tab)
 {
     int i;
-    for (i=0; i<size; i++) printf("%d\n",tab[i]);
+    for (i=0; i<(int)size; i++) printf("%d\t",tab[i]);
+}
+
+void insertion(int *tab)
+{
+    int j,i,x;
+    for(j = size - 2; j >= 0; j--)
+    {
+        x = tab[j];
+        i = j + 1;
+        while((i < size) && (x > tab[i]))
+        {
+            tab[i - 1] = tab[i];
+            i++;
+        }
+        tab[i - 1] = x;
+    }
+}
+
+int split(int *tab, int right, int left)
+{
+    int n;
+    n = left+(right-left)/2;
+    return n;
+}
+/////////////////AVL///////////////
+
+avlNode *newNode(int value)
+{
+    avlNode *node = (avlNode*) malloc(sizeof(avlNode));
+    node->val = value;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+avlNode *createAVL(int *tab, int left, int right)
+{
+    if (left > right) return NULL;
+    int n = left+(right-left)/2;
+    avlNode *root = newNode(tab[n]);
+    root->left = createAVL(tab, left, n-1);
+    root->right = createAVL(tab, n+1, right);
+    return root;
+}
+
+void preOrder(avlNode* root)
+{
+    if (root == NULL) return;
+    printf("%d\t",root->val);
+    preOrder(root->left);
+    preOrder(root->right);
 }
 
 int main()
 {
-    //int size,i;
     printf("Podaj liczbe elementow:\n");
     scanf("%d",&size);
-    size*=1.1;
     int *tab = (int*)malloc(sizeof(int)*size);
     losuj(tab);
     wyswietl(tab);
+    printf("\n-----------------\n");
+    ////AVL
+    insertion(tab);
+    avlNode *avlRoot = createAVL(tab, 0, size-1);
+    preOrder(avlRoot);
+
+    free(tab);
+
 }
